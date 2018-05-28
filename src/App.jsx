846166881444cf4d9d1f673ae5ea283300ff9efc
Brandon Wong <brandon@yerdle.com>
2018-05-28 16:49:34 -0700

    Add ability to mute people. Cleanup. This closes #1

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,74 @@ import React, { Component } from 'react';
 import Video from 'twilio-video';
 import './App.css';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzM1YWQ2YjA0NTA1NmQwOGI0MDI2ZmYyNTBlZjk1ODliLTE1Mjc1MjY2NTMiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJQZXJzb24gMiIsInZpZGVvIjp7InJvb20iOiJSb29tIDEifX0sImlhdCI6MTUyNzUyNjY1MywiZXhwIjoxNTI3NTMwMjUzLCJpc3MiOiJTSzM1YWQ2YjA0NTA1NmQwOGI0MDI2ZmYyNTBlZjk1ODliIiwic3ViIjoiQUMyY2IxNzM3OWNkMTBiODQxNzgxMTM1MTkyOGJkMzk0YiJ9.inCDgxSYq91N6Dq09OVjOpIn5VAbw6lA3GkwXYglMfk';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzM1YWQ2YjA0NTA1NmQwOGI0MDI2ZmYyNTBlZjk1ODliLTE1Mjc1NDgxMDMiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJQZXJzb24gMiIsInZpZGVvIjp7InJvb20iOiJSb29tIDEifX0sImlhdCI6MTUyNzU0ODEwMywiZXhwIjoxNTI3NTUxNzAzLCJpc3MiOiJTSzM1YWQ2YjA0NTA1NmQwOGI0MDI2ZmYyNTBlZjk1ODliIiwic3ViIjoiQUMyY2IxNzM3OWNkMTBiODQxNzgxMTM1MTkyOGJkMzk0YiJ9.twRyg3ER0rZBQaRKbXsIcseJkTyu5BeemYlnqXEtjmQ';
 
 class App extends Component {
+  state = {
+    participants: {},
+  };
+
   componentDidMount() {
     Video.connect(token, { name: 'Room 1' })
       .then((room) => {
-        console.log({ room });
-
-        console.log(room.participants);
-
-        room.on('participantConnected', (participant) => {
-          console.log(`Participant ${participant.identity} connected`);
-
-          participant.tracks.forEach((track) => {
-            console.log('each track', track);
-            document.getElementById('remote-media-div').appendChild(track.attach());
-          });
-
-          participant.on('trackAdded', (track) => {
-            const media = track.attach();
-            console.log(track);
-            console.log('added track', media);
-            if (track.kind === 'audio') {
-              media.pause();
-            }
-            document.getElementById('remote-media-div').appendChild(media);
-          });
+        room.participants.forEach((participant) => {
+          this.setListener(participant);
         });
       }, console.error);
   }
+
+  setListener = (participant) => {
+    participant.on('trackAdded', (track) => {
+      this.updateParticipant(participant, track);
+    });
+  }
+
+  updateParticipant = (participant, track) => {
+    const media = track.attach();
+    let state = { ...participant, ...this.state.participants[participant.sid] };
+    if (track.kind === 'audio') {
+      state = { ...participant, media, isMuted: true };
+      media.pause();
+    }
+    this.setState({ participants: { [participant.sid]: state } });
+    document.getElementById('remote-media-div').appendChild(media);
+  }
+
+  renderButton = ([sid, entry]) => {
+    const action = entry.media.paused ? 'Unmute' : 'Mute';
+    return (
+      <button
+        key={`Participant-${sid}`}
+        onClick={() => {
+          const { isMuted } = this.state.participants[sid];
+          if (isMuted) {
+            entry.media.play();
+          } else {
+            entry.media.pause();
+          }
+          this.setState({
+            participants: {
+              [sid]: {
+                ...this.state.participants[sid],
+                isMuted: !isMuted,
+              },
+            },
+          });
+        }}
+      >
+        {`${action} ${entry.identity}`}
+      </button>
+    );
+  }
+
 
   render() {
     return (
       <div>
         <div id="remote-media-div" />
+        <div className="mute">
+          {Object.entries(this.state.participants).map(this.renderButton)}
+        </div>
       </div>
     );
   }
