87decf7bd90a7daeb0a86f92983f540a45ddbf5a
Brian Wong <brian.lw.wong@gmail.com>
2018-06-11 10:30:57 -0500

    This closes #10. Input added to accept phone number and email to send to connect function

--- src/components/Identifier.jsx
+++ src/components/Identifier.jsx
@@ -3,17 +3,38 @@ import { connect } from '../service';
 import './Identifier.css';
 
 class Identifier extends Component {
-  state = {};
+  state = {
+    email: '',
+    phoneNumber: '',
+  };
+
+  update = (stateName, value) => {
+    this.setState({ [stateName]: value });
+  }
 
   handleClick = async () => {
-    const response = await connect();
+    const { phoneNumber, email } = this.state;
+    const response = await connect(phoneNumber, email);
     console.log('response', response);
   };
 
   render() {
     return (
       <div>
-        Identifier
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={event => this.update('email', event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Phone Number"
+            onChange={event => this.update('phoneNumber', event.target.value)}
+          />
+        </div>
         <button onClick={this.handleClick}>Connect</button>
       </div>
     );
